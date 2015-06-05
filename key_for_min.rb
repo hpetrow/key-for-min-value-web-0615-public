73f9aa0978f8a265c81f02b1c7f6c9293b5ceadf
hpetrow <heather.petrow@gmail.com>
2015-06-05 13:37:16 -0400

    done

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,14 @@
 
 def key_for_min_value(name_hash)
   # code goes here
+  if (name_hash.length == 0)
+    min_value_key = nil
+  else
+    min_value_key = name_hash.first[0]
+    min_value = name_hash.first[1]
+    name_hash.each { |key, value|
+      min_value_key = key if value < min_value
+    }
+  end
+  min_value_key
 end
